8c75b631279802579866de735b8cbc57da2fca65
ZAHRA <zahramosbal@gmail.com>
2023-09-07 10:02:18 -0700

    last one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
+/**
+ * This function is the entry point of the program.
+ */
 #include <stdio.h>
-
-int main(void) {
-puts(" \"Programming is like building a multilingual puzzle \"");
-return 0;
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
