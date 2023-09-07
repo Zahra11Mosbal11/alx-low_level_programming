af6b2194e4e08204fb0b3598934758e93b5cb72b
ZAHRA <zahramosbal@gmail.com>
2023-09-07 10:12:06 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,19 @@
 /**
- * This function is the entry point of the program.
+ * @brief The main function of the program.
+ *
+ * This function is the entry point of the program and is where
+ * the program's execution begins.
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv An array of strings containing the command-line arguments.
+ * @return Returns 0 on successful execution.
  */
 #include <stdio.h>
 int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
+/**
+ *  my code here
+ */
 return (0);
 }
