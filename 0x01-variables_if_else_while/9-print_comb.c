464d4e7ecb65ead14f5ba7a2a342113cb5501433
ZAHRA <zahramosbal@gmail.com>
2023-09-08 08:12:30 -0700

    all possible

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int numberz;
+	/* Variable declaration  */
+
+	/* your code goes there */
+
+	for (numberz = 48; numberz <= 57; numberz++)
+	{
+		putchar(numberz);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
