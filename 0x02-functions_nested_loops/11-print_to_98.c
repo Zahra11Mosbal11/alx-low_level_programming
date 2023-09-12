2c449863cb2d042b226ab1883851e191cf604339
ZAHRA <zahramosbal@gmail.com>
2023-09-12 16:38:48 -0700

    donz

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,20 +1,39 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_to_98 - Entry point
- *
+ *@n: for print number.
 */
 void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-		_putchar(i / 10 % 10 + '0');
-		_putchar(i % 10 + '0');
-		if (i != 98)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
 		}
 	}
 	_putchar('\n');
