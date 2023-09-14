1d4034fe7093c3afa630925509e7add663459e6e
ZAHRA <zahramosbal@gmail.com>
2023-09-14 03:27:25 -0700

    test

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ *
+ * @a: the first number will mul
+ * @b:the seceand number will mul
+ * Return: intgeger number
+*/
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
