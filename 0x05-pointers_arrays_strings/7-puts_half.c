ff336ceb72e8b1d6d201fb7807e0512620f1b661
ZAHRA <zahramosbal@gmail.com>
2023-09-18 05:11:55 -0700

    test

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,6 +10,14 @@ void puts_half(char *str)
 	int langth = strlen(str);
 	int half;
 
+	if (langth % 2 != 0)
+	{
+		for (half = (langth - 1) / 2; half < langth; half++)
+		{
+			_putchar(str[half]);
+		}
+		_putchar('\n');
+	}
 	for (half = (langth) / 2; half < langth; half++)
 	{
 		_putchar(str[half]);
