2059d8310dc2ca76aa97e01732123606bdc49841
ZAHRA <zahramosbal@gmail.com>
2023-09-18 00:19:45 -0700

    2th done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strlen - Entry point
+ *@s: array of characters
+ * Return: the length of a string.
+*/
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+
+	return (length);
+}
