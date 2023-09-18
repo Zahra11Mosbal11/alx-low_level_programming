02cf0fe3dc4387163f63c0d732a8ca242053bbac
ZAHRA <zahramosbal@gmail.com>
2023-09-18 06:10:33 -0700

    9th done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - Entry point
+ *@src: cope from
+ *@dest: cope to it
+ * Return: char valuo
+*/
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
