5c32aa499ae3f3734185b53cfeb2dacdb6ee942d
ZAHRA <zahramosbal@gmail.com>
2023-09-18 01:03:05 -0700

    4th done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - Entry point
+ *@s: string will revers
+ * Return: void
+*/
+void print_rev(char *s)
+{
+	int langth = strlen(s);
+	int i;
+
+	for (i = langth; i >= '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
