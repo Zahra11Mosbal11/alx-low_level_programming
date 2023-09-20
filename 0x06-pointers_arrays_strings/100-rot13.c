9948cdb41bc13797337023ff751fc3f4e6edda99
ZAHRA <zahramosbal@gmail.com>
2023-09-20 07:46:59 -0700

    100th done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+/**
+ *rot13  - Entry point
+ *
+ *@s: the second string
+ *Return: s string
+*/
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (isalpha(s[i]))
+		{
+			char base = islower(s[i]) ? 'a' : 'A';
+			s[i] = ((s[i] - base + 13) % 26) + base;
+		}
+	}
+	return (s);
+}
