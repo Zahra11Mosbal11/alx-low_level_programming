44867bf18838c1da33ebbf6b6d1f1157fa73b008
ZAHRA <zahramosbal@gmail.com>
2023-09-19 21:50:37 -0700

    3th done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - Entry point
+ *
+ *@s1: the first string
+ *@s2: the second string
+ *Return: val string
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int val;
+
+	val = strcmp(s1, s2);
+
+	return (val);
+}
