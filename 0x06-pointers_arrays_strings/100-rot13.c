6c26e9037652e539b53483ebd6677281886c8216
ZAHRA <zahramosbal@gmail.com>
2023-09-21 09:30:09 -0700

    E A

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,25 +9,22 @@
 */
 char *rot13(char *s)
 {
-	int i, j;
-	char up[] = "aAbBcCdDeEfFgGhHiIjJkKlLmM";
-	char done[] = "nNoOpPqQrRsStTuUvVwWhHyYzZ";
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 26; j++)
+		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == up[j])
+			if (s[i] == data1[j])
 			{
-				s[i] = done[j];
-				break;
-			}
-			else if (s[i] == done[j])
-			{
-				s[i] = up[j];
+				s[i] = datarot[j];
 				break;
 			}
 		}
 	}
+
 	return (s);
 }
