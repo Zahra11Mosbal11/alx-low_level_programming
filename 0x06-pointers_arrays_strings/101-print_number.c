fd686c384474a3362db8a5180ac463e118efa5b8
ZAHRA <zahramosbal@gmail.com>
2023-09-21 09:37:11 -0700

    done

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,18 @@
 */
 void print_number(int n)
 {
+	unsigned int N;
+	
+	N = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		N = -n;
 	}
-	if (n / 10 != 0)
+	if (N / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(N / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(N % 10 + '0');
 }
