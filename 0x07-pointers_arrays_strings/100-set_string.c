8373be4ffef67a1c42b733fe78f7bc854a0098fe
ZAHRA <zahramosbal@gmail.com>
2023-09-25 08:21:00 -0700

    test1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * set_string - Entry point
+ *
+ *@to: the first string
+ *@s: the second string
+ *Return: void
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
