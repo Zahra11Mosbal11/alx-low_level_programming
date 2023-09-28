2c8eb3e967b099f3d8aa178ab21e0f1184e0f8ed
ZAHRA <zahramosbal@gmail.com>
2023-09-28 02:41:44 -0700

    test

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * factorial - Entry point
+ *
+ *@n: the number.
+ *Return: -1 if anegatev number and 1 if 1 or 0.
+ *Return: n integer
+*/
+int factorial(int n)
+{
+	int N;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n <= 1)
+	{
+		N = 1;
+		return (N);
+	}
+	else
+	{
+		N = n * factorial(n - 1);
+		return (N);
+	}
+}
