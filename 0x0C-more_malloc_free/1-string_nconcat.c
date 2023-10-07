e0859c9e12247d888a5b785dd1631c527fcf6e87
ZAHRA <zahramosbal@gmail.com>
2023-10-07 09:56:10 -0700

    tetst

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,6 +12,9 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
+	int i, j;
+	int N = n;
+	int len1 = strlen(s1);
 	int len = strlen(s1) + n + 1;
 
 	if (s1 == NULL || s2 == NULL)
@@ -24,7 +27,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	strcpy(ptr, s1);
-	strncat(ptr, s2, n);
+
+	for (i = 0; i < len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+
+	for (j = 0; j < N && s2[j] != '\0'; j++)
+	{
+		ptr[i + j] = s2[j];
+	}
+	ptr[i + j] = '\0';
 	return (ptr);
 }
