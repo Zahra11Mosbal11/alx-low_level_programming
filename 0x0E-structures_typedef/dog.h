4af882791f2a532760b7d24a63b2a1461edf1b22
ZAHRA <zahramosbal@gmail.com>
2023-10-10 05:41:56 -0700

    test

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef MYSTRUCT_H
 #define MYSTRUCT_H
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 
 struct dog
 {
