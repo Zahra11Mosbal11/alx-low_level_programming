fb81cb797a1c9182e657356d153fc6a49fde2215
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-10-23 04:29:35 -0700

    5 done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - sart
+ *
+ * @head: the node
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (*head == NULL)
+		return;
+	while (*head)
+	{
+		node = (*head)->next;
+		free(*head);
+		*head = node;
+	}
+	*head = NULL;
+}
