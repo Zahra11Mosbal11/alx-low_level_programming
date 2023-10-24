eff645de2c1f2ee6d3336537d375146a900a9f87
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-10-24 02:54:51 -0700

    test10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - start
+ *
+ * @head: the node
+ * @index: the indx
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *node = *head;
+	listint_t *current = NULL;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(node);
+		return (1);
+	}
+	while (i < index - 1)
+	{
+		if (!node || !(node->next))
+			return (-1);
+		node = node->next;
+		i++;
+	}
+
+	current = node->next;
+	node->next = current->next;
+	free(current);
+
+	return (1);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	node->n = n;
+	node->next = NULL;
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
 
 	while (curnt && i < idx)
 	{
