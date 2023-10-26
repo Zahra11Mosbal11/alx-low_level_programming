d389a950081818318eac1b64ea60c0b3d745e5e3
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-10-26 02:28:55 -0700

    0 done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * binary_to_uint - Entry point
+ *@b: the binary number
+ * Return: unsigned int
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		decimal = decimal * 2 + (b[i] - '0');
+	}
+	return (decimal);
+}
