50ff508fce4a43f707f89a06ba3217a7d3ef58ed
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-10-30 02:51:02 -0700

    1 done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+/**
+ * create_file - start
+ * @filename: is the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fp = open(filename, O_CREAT | O_WRONLY, 0600);
+	ssize_t length, writf;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		length = strlen(text_content);
+		writf = write(fp, text_content, length);
+		if (writf == -1 || writf != length)
+		{
+			close(fp);
+			return (-1);
+		}
+	}
+	close(fp);
+	return (1);
+}
