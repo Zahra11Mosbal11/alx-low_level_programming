9f15dad0287014c5059cb98909faa68d6b27c742
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-10-30 07:28:24 -0700

    test2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,17 +22,15 @@ void error_handil(int code, char *masg)
 int main(int argc, char *argv[])
 {
 	int fs, fd;
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
 	ssize_t readf, writef;
 	char buf[BUFFER_SIZE];
 
 	if (argc != 3)
 		error_handil(97, "Usage: cp file_from file_to");
-	fs = open(file_from, O_RDONLY);
+	fs = open(argv[1], O_RDONLY);
 	if (fs == -1)
 		error_handil(98, "Error: Can't read from file");
-	fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
+	fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (fd == -1)
 		error_handil(99, "Error: Can't write to");
 	while ((readf = read(fs, buf, BUFFER_SIZE)) > 0)
