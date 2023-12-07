b82409eded904bf8016ba80d2d5cf3987abd4043
Zahra11Mosbal11 <zahramosbal@gmail.com>
2023-12-07 08:45:31 -0800

    node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,11 +10,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *node = *head, *curnt = NULL;
 	unsigned int i = 0;
 
-	if (!*head)
+	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
-		*head = (*head)->next;
+		*head = node->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		free(node);
 		return (1);
 	}
